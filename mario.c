a935dd3e0a4c959d4dba9b6558f2fde1d706b9af
Larry Adigun <larryadigun.student@careerdevs.com>
2019-05-06 14:29:53 +0000

    first commit

--- mario.c
+++ mario.c
@@ -10,14 +10,17 @@ do
     //Asking the user to write in a height
      height = get_int("Please put in a height:");
    }
+   //Loop through the and se if the height given by the user is between the threshold given.
 
     while (height < 1 || height > 8);
     for (int i = 0; i < height; i++)
+    //creating the space for the hash symbols to the right
     {
         for (int sp = height - i; sp > 1; sp--)
         {
             printf(" ");
         }
+        //
         for (int hashes = 0; hashes < i + 1; hashes++)
         {
             printf("#");
