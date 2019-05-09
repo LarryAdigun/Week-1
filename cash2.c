fdb1317e7487ca02e6ce03711e499d7f352e62bb
Larry Adigun <larryadigun.student@careerdevs.com>
2019-05-09 14:52:16 +0000

    first commit

--- cash2.c
+++ cash2.c
@@ -11,3 +11,26 @@ int main(void)
     }
     while (dollars < 0); // If the user plugs in a neg # goes back to the top
     int cents = round(dollars * 100);
+
+    int coins = 0; //counter for the numbe of coins
+    while (cents >= 25) // loop evaluates only when the cents is greater than 25. For example 35-25 = 10 loop ended.
+    {
+        cents -= 25;
+        coins++;
+    }
+    while (cents >= 10)
+    {
+        cents -= 10;
+        coins++;
+    }
+
+    while (cents >= 5)
+    {
+        cents -= 5;
+        coins++;
+    }
+    while (cents >= 1)
+    {
+        cents -= 1;
+        coins++;
+    }
